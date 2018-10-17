2356a3c860eb436ec3cfc395f10a790f3b5ab75c
Toch <tochukwu.claver@gmail.com>
2018-10-17 00:38:25 -0400

    liking grid css

--- src/app/public/home/components/header/header.component.ts
+++ src/app/public/home/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Slides } from '@ionic/angular';
+import { Slides, Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 
@@ -11,15 +11,18 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   public photo = 'url(../../../../../../../assets/imgs/stacy_pro_phone.png)';
-
+  public photoStyle: { [key: string]: string };
 
   constructor(
-    private router: Router
+    private router: Router,
+    private platform: Platform
   ) { }
 
 
   ngOnInit() {
-
+    this.photoStyle = {
+      'height':  `${ this.platform.height()}`
+    };
   }
   onSlideLoad(slides: Slides) {
     slides.startAutoplay();
